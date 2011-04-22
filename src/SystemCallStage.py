6b8d791ab47d9134b5620b03080d0ab133cc841b
Francesco Pierfederici <fpierfed@stsci.edu>
2011-04-22 17:07:34 -0400

    Implemented SystemCallStage

--- src/SystemCallStage.py
+++ src/SystemCallStage.py
@@ -26,10 +26,80 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 # DAMAGE.
+import subprocess
+import traceback
+
 from Stage import Stage
 
 
 
 
+
 class SystemCallStage(Stage):
-    pass
+    """
+    Execute the command given in the configuration file in a system call and
+    potentially log the command STDOUT and/or STDERR. If requested, the exit 
+    code of the Stage process method is that of the command being executed. 
+    Otherwise it is whether or not the system call itself suceeded.
+    """
+    def process(self):
+        """
+        Execute the command given in the parameters section of teh configuration
+        file in a system call. These are the relevant parameters:
+            self.command: the command to execute. This is the same as executable
+                in subprocess.Popen()
+            self.arguments: the argument list to self.command. It can be empty.
+            self.log_stdout: boolean. If True, pipe the command STDOUT to 
+                self.log
+            self.log_stderr: boolean. If True, pipe the command STDERR to 
+                self.log
+            self.exitcode_passthrough: boolean. If True the return value of 
+                self.process() is the exit code of self.process. If False it is
+                the return code of the system call itself (usually 0). Setting 
+                this parameter to False is useful in cases where self.command 
+                could fail but we do not want the Pipeline to stop.
+        """
+        cmd_str = '%s %s' % (self.command, ' '.join(self.arguments))
+        
+        # Start the process and wait for it to finish.
+        self.log.info('Starting "%s"' % (cmd_str))
+        try:
+            p = subprocess.Popen(args=[self.command, ] + self.arguments,
+                                 stdin=None,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 shell=False)
+            err = p.wait()
+        except:
+            # We got an exception. This is a problem unless 
+            # self.exitcode_passthrough == False, in whiuch case we do not care.
+            msg = 'The execution of "%s" failed with an exception: \n%s' \
+                  % (cmd_str, traceback.format_exc())
+            self.log.info(msg)
+            
+            if(not self.exitcode_passthrough):
+                self.log.info('exitcode_passthrough=false: errors are ignored.')
+                return(0)
+            else:
+                raise(Exception(msg))
+        
+        self.log.info('"%s" command done.' % (self.command))
+        
+        # Log STDOUT/ERR if we are asked to do so.
+        if(self.log_stdout):
+            self.log.info('"%s" STDOUT: "%s"' % (self.command, p.stdout.read()))
+        if(self.log_stderr):
+            self.log.info('"%s" STDERR: "%s"' % (self.command, p.stderr.read()))
+        
+        # Return err or 0?
+        if(self.exitcode_passthrough):
+            return(err)
+        return(0)
+
+
+
+
+
+
+
+
